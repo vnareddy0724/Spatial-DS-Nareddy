39cc71f32c5b6a713b6d59770d966d414b9d930a
vnareddy0724 <vahini.5678@gmail.com>
2017-06-27 20:01:40 -0500

    Update generate_country_geojson.py

--- Assignments/program_4/generate_country_geojson.py
+++ Assignments/program_4/generate_country_geojson.py
@@ -1,1 +1,28 @@
 
+import pprint as pp
+import os,sys
+import json
+import collections
+
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
+
+f = open(DIRPATH+'./countries.geo.json',"r")
+
+data = f.read()
+
+data = json.loads(data)
+
+all_country = []
+
+range=1
+
+del data [999: len(data)-1]
+	
+
+	
+out = open(DIRPATH+'./geo_json/countries_geo_json.geojson',"w")
+
+out.write(json.dumps(data, sort_keys=False,indent=4, separators=(',', ': ')))
+
+out.close()	
+		
