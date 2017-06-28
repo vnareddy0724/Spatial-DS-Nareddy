c9227ab09da063969fc13d1509fec7267a447d8d
vnareddy0724 <vahini.5678@gmail.com>
2017-06-27 20:00:18 -0500

    Update generate_states_geojson.py

--- Assignments/program_4/generate_states_geojson.py
+++ Assignments/program_4/generate_states_geojson.py
@@ -1,1 +1,43 @@
 
+import pprint as pp
+import os,sys
+import json
+import collections
+
+DIRPATH = os.path.dirname(os.path.realpath(__file__))
+
+f = open(DIRPATH+'./state_borders.json',"r")
+
+data = f.read()
+
+data = json.loads(data)
+
+all_states = []
+
+#print(type(data))
+
+range=1
+
+for i in data:
+	state={}
+	state['type']="Feature"
+	state['properties']=i
+	state['geometry']={}
+	state['geometry']['type']="Polygon"
+	state['geometry']['coordinates']=state['properties']['borders']
+	del state['properties']['borders']
+	all_states.append(state)
+	range=range+1
+	if range == 1001:
+		break
+
+	
+		
+	
+out = open(DIRPATH+'./geo_json/statesborders_geo_json.geojson',"w")
+
+out.write(json.dumps(all_states, sort_keys=False,indent=4, separators=(',', ': ')))
+
+out.close()	
+	
+			
